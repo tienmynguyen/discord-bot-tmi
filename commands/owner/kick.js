4a7912a86f3001eb1ac69669bcacc5560f6289d9
Nguyễn Tiến Mỹ <118043490+tienmynguyen@users.noreply.github.com>
2025-07-28 22:22:26 +0700

    Update kick.js

--- commands/owner/kick.js
+++ commands/owner/kick.js
@@ -1,21 +1,29 @@
-const { EmbedBuilder } = require("discord.js");
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = {
-  name: "kick",
-  category: "owner",
-  run: (client, message, args) => {
-    const member = message.mentions.members.first();
+    name: "kick",
+    category: "owner",
+    run: (client, message, args) => {
+        const member = message.mentions.members.first();
 
-    try {
-      if (member) {
-        const memberTaget = message.guild.members.cache.get(member.id);
-        memberTaget.kick();
-        message.channel.send("hơi thở của staff thức thứ 1, búng tay");
-      } else {
-        message.channel.send("bạn í mạnh hơn mình :/");
-      }
-    } catch (error) {
-      message.channel.send("bạn í mạnh hơn mình :/");
-    }
-  },
+        // Check nếu người dùng không có quyền
+        if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+            return message.reply("Bạn không có quyền để đuổi người khác!");
+        }
+
+        // Check nếu bot không có quyền
+        if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+            return message.reply("Bot không có quyền để kick người!");
+        }
+
+        if (!member) return message.reply("Bạn cần mention người cần kick!");
+
+        try {
+            member.kick();
+            message.channel.send("hơi thở của staff thức thứ 1, búng tay");
+        } catch (error) {
+            console.error(error);
+            message.channel.send("Không thể kick người này. Có thể họ mạnh quá 🫡");
+        }
+    },
 };
